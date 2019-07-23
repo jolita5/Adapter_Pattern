97285675b5cbb69daea7a85397269897c70a95e4
jolita5 <jolita.jezukeviciute@gmail.com>
2019-07-23 10:51:22 +0300

    Adapter pattern executed

---
+++ Adapter/Adapter/ChemicalDatabank.cs
@@ -0,0 +1,53 @@
+﻿using System.Data.Odbc;
+
+namespace Adapter
+{
+    class ChemicalDatabank
+    {
+        public float GetCriticalPoint(string compound, string point)
+        {
+            if (point == "M")
+            {
+                switch (compound.ToLower())
+                {
+                    case "water": return 0.0f;
+                    case "benzene": return 5.5f;
+                    case "ethanol": return -114.4f;
+                    default: return 0f;
+                }
+            }
+            else
+            {
+                switch (compound.ToLower())
+                {
+                    case "water": return 100.0f;
+                    case "benzene": return 80.1f;
+                    case "ethanol": return 78.3f;
+                    default: return 0f;
+                }
+            }
+        }
+
+        public string GetMolecularStructure(string compound)
+        {
+            switch (compound.ToLower())
+            {
+                case "water": return "H20";
+                case "benzene": return "C6H6";
+                case "ethanol": return "C2H50H";
+                default: return "";
+            }
+        }
+
+        public double GetMolecularWeight(string compound)
+        {
+            switch (compound.ToLower())
+            {
+                case "water": return 18.015;
+                case "benzene": return 78.1134;
+                case "ethanol": return 46.0688;
+                default: return 0d;
+            }
+        }
+    }
+}
---
+++ Adapter/Adapter/Compound.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Adapter
+{
+    class Compound
+    {
+        protected string _chemical;
+        protected float _boilingPoint;
+        protected float _meltingPoint;
+        protected double _molecularWeight;
+        protected string _molecularFormula;
+
+        public Compound(string chemical)
+        {
+            _chemical = chemical;
+        }
+
+        public virtual void Display()
+        {
+            Console.WriteLine("\nCompound: {0} ------ ", _chemical);
+        }
+    }
+}
